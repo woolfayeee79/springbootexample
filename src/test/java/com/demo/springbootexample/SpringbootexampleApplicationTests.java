48fbfb46a8c6bab90bc89f24048f67ee26dbd87b
woolfayeee79 <meganwoolschlager766@gmail.com>
2020-01-21 21:18:29 -0600

    first commit

---
+++ src/test/java/com/demo/springbootexample/SpringbootexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
